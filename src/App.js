0264ff419f0c5b3612272d63914c7392e3a5e823
duvaplza <duvan.apiolaza@usm.cl>
2024-06-14 19:30:14 -0400

    Juan

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React - Esta es mi app</h1>
+        <h1>Esta es mi app</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
